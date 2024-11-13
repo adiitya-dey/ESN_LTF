79f7071ab67f7daca4511d7dc43eabf545bf473f
aditya.dey <adiitya.dey@gmail.com>
2024-11-13 12:05:05 +0100

    L1 + MSE loss

--- utils/tools.py
+++ utils/tools.py
@@ -126,11 +126,12 @@ def test_params_flop(model,x_shape):
         return macs, params
     
 class WaveletMSELoss(nn.Module):
-    def __init__(self, alpha=1.0, beta=3.0):
+    def __init__(self, alpha=10.0, beta=1.0):
         super(WaveletMSELoss, self).__init__()
         self.low_pass_filter = torch.tensor([1, 1], dtype=torch.float32) / math.sqrt(2)
         self.high_pass_filter = torch.tensor([-1, 1], dtype=torch.float32) / math.sqrt(2)
-        self.criterion = nn.MSELoss()
+        self.criterion1 = nn.MSELoss()
+        self.criterion2 = nn.L1Loss()
         self.alpha = nn.Parameter(torch.tensor(alpha))
         self.beta = nn.Parameter(torch.tensor(beta))
 
@@ -149,8 +150,8 @@ class WaveletMSELoss(nn.Module):
         y_pred_D = F.conv1d(input=y_pred, weight=high_pass, stride=2, groups=channel)
         y_true_D = F.conv1d(input=y_true, weight=high_pass, stride=2, groups=channel)
         
-        loss_approx = self.criterion(y_pred_A.permute(0,2,1), y_true_A.permute(0,2,1))
-        loss_detail = self.criterion(y_pred_D.permute(0,2,1), y_true_D.permute(0,2,1))
+        loss_approx = self.criterion2(y_pred_A.permute(0,2,1), y_true_A.permute(0,2,1))
+        loss_detail = self.criterion1(y_pred_D.permute(0,2,1), y_true_D.permute(0,2,1))
         total_loss = self.alpha * loss_approx + self.beta * loss_detail
 
         return total_loss
