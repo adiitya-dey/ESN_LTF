6c124b9c886101a6ea855cec0bbf6d32d0ddcfd3
aditya.dey <adiitya.dey@gmail.com>
2024-11-13 14:04:23 +0100

    testing DLinear

--- models/LTF.py
+++ models/LTF.py
@@ -1,136 +1,86 @@
+from re import X
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
-from torch.autograd import Function
-from scipy.fft import dct, idct
-import math
 
-from layers.LowRankLinear import ThinLinear, ReducedLinear, AnotherLinear
+class moving_avg(nn.Module):
+    """
+    Moving average block to highlight the trend of time series
+    """
+    def __init__(self, kernel_size, stride):
+        super(moving_avg, self).__init__()
+        self.kernel_size = kernel_size
+        self.avg = nn.AvgPool1d(kernel_size=kernel_size, stride=stride, padding=0)
 
-# class DCT(Function):
-#         @staticmethod
-#         def forward(ctx, input):
-#             # Convert PyTorch tensor to NumPy array
-#             input_np = input.cpu().numpy()
-#             # Apply DCT using scipy
-#             transformed_np = dct(input_np, type=2, norm="ortho", axis=-1, orthogonalize=True)
-#             # Convert back to PyTorch tensor
-#             output = torch.from_numpy(transformed_np).to(input.device)
-#             return output
-
-#         @staticmethod
-#         def backward(ctx, grad_output):
-#             # Convert gradient to NumPy array
-#             grad_output_np = grad_output.cpu().numpy()
-#             # Apply IDCT using scipy
-#             grad_input_np = idct(grad_output_np, type=2, norm='ortho', axis=-1, orthogonalize=True)
-#             # Convert back to PyTorch tensor
-#             grad_input = torch.from_numpy(grad_input_np).to(grad_output.device)
-#             return grad_input
-
-
-class IDCT(Function):
-    @staticmethod
-    def forward(ctx, input):
-        # Convert PyTorch tensor to NumPy array
-        input_np = input.cpu().numpy()
-        # Apply IDCT using scipy
-        transformed_np = idct(input_np, type=2, axis=-1)
-        # Convert back to PyTorch tensor
-        output = torch.from_numpy(transformed_np).to(input.device)
-        return output
-
-    @staticmethod
-    def backward(ctx, grad_output):
-        # Convert gradient to NumPy array
-        grad_output_np = grad_output.cpu().numpy()
-        # Apply DCT using scipy
-        grad_input_np = dct(grad_output_np, type=2, axis=-1)
-        # Convert back to PyTorch tensor
-        grad_input = torch.from_numpy(grad_input_np).to(grad_output.device)
-        return grad_input
-
-
-class FFN(nn.Module):
-    def __init__(self, input_size, hidden_size, output_size):
-        super(FFN, self).__init__()
-        self.fc1 = nn.Linear(input_size, hidden_size)  # Input layer to hidden layer
-        self.fc2 = nn.Linear(hidden_size, output_size)  # Hidden layer to output layer
-
-
-    # x: [batch, input_seg, reservoir size]
     def forward(self, x):
-        out = self.fc1(x)  # Linear transformation
-        out = self.fc2(out)  # Linear transformation
-        return out
+        # padding on the both ends of time series
+        front = x[:, 0:1, :].repeat(1, (self.kernel_size - 1) // 2, 1)
+        end = x[:, -1:, :].repeat(1, (self.kernel_size - 1) // 2, 1)
+        x = torch.cat([front, x, end], dim=1)
+        x = self.avg(x.permute(0, 2, 1))
+        x = x.permute(0, 2, 1)
+        return x
+
+
+class series_decomp(nn.Module):
+    """
+    Series decomposition block
+    """
+    def __init__(self, kernel_size):
+        super(series_decomp, self).__init__()
+        self.moving_avg = moving_avg(kernel_size, stride=1)
+
+    def forward(self, x):
+        moving_mean = self.moving_avg(x)
+        res = x - moving_mean
+        return res, moving_mean
 
 class Model(nn.Module):
     """
-    Just one Linear layer
+    DLinear
     """
     def __init__(self, configs):
         super(Model, self).__init__()
         self.seq_len = configs.seq_len
         self.pred_len = configs.pred_len
 
+        # Decompsition Kernel Size
+        kernel_size = 25
+        self.decompsition = series_decomp(kernel_size)
+        self.individual = configs.individual
         self.channels = configs.enc_in
 
-        self.low_pass_filter = torch.tensor([1, 1], dtype=torch.float32) / math.sqrt(2)
-
-        self.low_pass_filter = self.low_pass_filter.reshape(1,1,-1).repeat(self.channels, 1, 1)
-
-        if (self.seq_len%2)!=0:
-            in_len = self.seq_len//2 + 1
+        if self.individual:
+            self.Linear_Seasonal = nn.ModuleList()
+            self.Linear_Trend = nn.ModuleList()
+            self.Linear_Decoder = nn.ModuleList()
+            for i in range(self.channels):
+                self.Linear_Seasonal.append(nn.Linear(self.seq_len,self.pred_len))
+                self.Linear_Seasonal[i].weight = nn.Parameter((1/self.seq_len)*torch.ones([self.pred_len,self.seq_len]))
+                self.Linear_Trend.append(nn.Linear(self.seq_len,self.pred_len))
+                self.Linear_Trend[i].weight = nn.Parameter((1/self.seq_len)*torch.ones([self.pred_len,self.seq_len]))
+                self.Linear_Decoder.append(nn.Linear(self.seq_len,self.pred_len))
         else:
-            in_len = self.seq_len//2
-
-        ## Create DCT matrix
-        identity_mat = np.eye(in_len)
-        dct_mat = dct(identity_mat, type=2, axis=0, norm="ortho")
-        self.dct_matrix = torch.tensor(dct_mat, dtype=torch.float)
-
-        self.layer_lo = nn.Linear(in_len,self.pred_len)
-        # self.layer_lo = ThinLinear(in_features=in_len,
-        #                            out_features=self.pred_len,
-        #                            rank=35,
-        #                            bias=True)
-        # self.layer_lo = ReducedVanillaLinear(in_features=in_len,
-        #                            out_features=self.pred_len,
-        #                            rank=35,
-        #                            bias=True)
-        # self.layer_lo = AnotherLinear(in_features=in_len,
-        #                               out_features=self.pred_len,
-        #                               rank=8)
-
-
+            self.Linear_Seasonal = nn.Linear(self.seq_len,self.pred_len)
+            self.Linear_Trend = nn.Linear(self.seq_len,self.pred_len)
+            self.Linear_Decoder = nn.Linear(self.seq_len,self.pred_len)
+            self.Linear_Seasonal.weight = nn.Parameter((1/self.seq_len)*torch.ones([self.pred_len,self.seq_len]))
+            self.Linear_Trend.weight = nn.Parameter((1/self.seq_len)*torch.ones([self.pred_len,self.seq_len]))
 
     def forward(self, x):
         # x: [Batch, Input length, Channel]
-        batch, _, _ = x.shape
+        seasonal_init, trend_init = self.decompsition(x)
+        seasonal_init, trend_init = seasonal_init.permute(0,2,1), trend_init.permute(0,2,1)
+        if self.individual:
+            seasonal_output = torch.zeros([seasonal_init.size(0),seasonal_init.size(1),self.pred_len],dtype=seasonal_init.dtype).to(seasonal_init.device)
+            trend_output = torch.zeros([trend_init.size(0),trend_init.size(1),self.pred_len],dtype=trend_init.dtype).to(trend_init.device)
+            for i in range(self.channels):
+                seasonal_output[:,i,:] = self.Linear_Seasonal[i](seasonal_init[:,i,:])
+                trend_output[:,i,:] = self.Linear_Trend[i](trend_init[:,i,:])
+        else:
+            seasonal_output = self.Linear_Seasonal(seasonal_init)
+            trend_output = self.Linear_Trend(trend_init)
 
-        ## Scaled Normalization
-        x = x.permute(0,2,1)
-        seq_mean = torch.mean(x, axis=-1, keepdim=True)
-        x = (x - seq_mean)
-
-        if (self.seq_len%2)!=0:
-            x = F.pad(x, (0, 1))
-
-        ## Haar decomposition
-        x = F.conv1d(input=x, weight=self.low_pass_filter, stride=2, groups=self.channels)
-
-        ## Cosine Transform
-        x = x @ self.dct_matrix / x.shape[-1]
-
-        ## Prediction
-        out = self.layer_lo(x)
-
-        out = out + seq_mean
-
-        return out.permute(0,2,1) # [Batch, Output length, Channel]
-
-
-    def step(self):
-        # self.layer_lo.step()
-        pass
+        x = seasonal_output + trend_output
+        return x.permute(0,2,1) # to [Batch, Output length, Channel]
