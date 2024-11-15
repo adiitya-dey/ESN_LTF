8fb36906e3a826dacafe624adaced9338d79e51e
aditya.dey <adiitya.dey@gmail.com>
2024-11-15 21:39:07 +0100

    removing Wavelet Loss.

--- exp/exp_main.py
+++ exp/exp_main.py
@@ -1,7 +1,7 @@
 from data_provider.data_factory import data_provider
 from exp.exp_basic import Exp_Basic
 from models import LTF
-from utils.tools import EarlyStopping, adjust_learning_rate, visual, test_params_flop, WaveletMSELoss
+from utils.tools import EarlyStopping, adjust_learning_rate, visual, test_params_flop
 from utils.metrics import metric
 
 
