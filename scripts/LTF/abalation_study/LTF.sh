fcf6ecd13edac92a5dea81886f73d082870e1e4a
aditya.dey <adiitya.dey@gmail.com>
2024-11-15 21:19:21 +0100

    Converted folder name to abalation_study.

---
+++ scripts/LTF/abalation_study/LTF.sh
@@ -0,0 +1,107 @@
+if [ ! -d "./logs" ]; then
+    mkdir ./logs
+fi
+
+model_name=LTF
+root_path_name=./dataset/
+
+data_path_name=ETTh1.csv
+model_id_name=ETTh1
+data_name=ETTh1
+
+for seq_len in 512
+do
+for pred_len in 96 192 336 720
+do
+  python -u run_longExp.py \
+    --is_training 1 \
+    --root_path $root_path_name \
+    --data_path $data_path_name \
+    --model_id $model_id_name'_'$seq_len'_'$pred_len \
+    --model $model_name \
+    --data $data_name \
+    --features S \
+    --seq_len $seq_len \
+    --pred_len $pred_len \
+    --enc_in 1 \
+    --train_epochs 100 \
+    --patience 20 \
+    --itr 1 --batch_size 32 --learning_rate 0.01
+done
+done
+
+
+data_path_name=ETTm2.csv
+model_id_name=ETTm2
+data_name=ETTm2
+
+for seq_len in 512
+do
+for pred_len in 96 192 336 720
+do
+  python -u run_longExp.py \
+    --is_training 1 \
+    --root_path $root_path_name \
+    --data_path $data_path_name \
+    --model_id $model_id_name'_'$seq_len'_'$pred_len \
+    --model $model_name \
+    --data $data_name \
+    --features S \
+    --seq_len $seq_len \
+    --pred_len $pred_len \
+    --enc_in 1 \
+    --train_epochs 100 \
+    --patience 20 \
+    --itr 1 --batch_size 32 --learning_rate 0.01
+done
+done
+
+data_path_name=traffic.csv
+model_id_name=traffic
+data_name=custom
+
+for seq_len in 512
+do
+for pred_len in 96 192 336 720
+do
+  python -u run_longExp.py \
+    --is_training 1 \
+    --root_path $root_path_name \
+    --data_path $data_path_name \
+    --model_id $model_id_name'_'$seq_len'_'$pred_len \
+    --model $model_name \
+    --data $data_name \
+    --features S \
+    --seq_len $seq_len \
+    --pred_len $pred_len \
+    --enc_in 1 \
+    --train_epochs 100 \
+    --patience 20 \
+    --itr 1 --batch_size 32 --learning_rate 0.01
+done
+done
+
+data_path_name=electricity.csv
+model_id_name=electricity
+data_name=custom
+
+for seq_len in 512
+do
+for pred_len in 96 192 336 720
+do
+  python -u run_longExp.py \
+    --is_training 1 \
+    --root_path $root_path_name \
+    --data_path $data_path_name \
+    --model_id $model_id_name'_'$seq_len'_'$pred_len \
+    --model $model_name \
+    --data $data_name \
+    --features S \
+    --seq_len $seq_len \
+    --pred_len $pred_len \
+    --enc_in 1 \
+    --train_epochs 100 \
+    --patience 20 \
+    --itr 1 --batch_size 32 --learning_rate 0.01
+done
+done
