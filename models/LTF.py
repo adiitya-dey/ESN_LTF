70fb973c42cfa785bf034c8a147418f67acbf172
aditya.dey <adiitya.dey@gmail.com>
2024-11-15 22:03:58 +0100

    scipy DCT.

--- models/LTF.py
+++ models/LTF.py
@@ -8,48 +8,48 @@ import math
 
 from layers.LowRankLinear import ThinLinear, ReducedLinear, AnotherLinear
 
-# class DCT(Function):
-#         @staticmethod
-#         def forward(ctx, input):
-#             # Convert PyTorch tensor to NumPy array
-#             input_np = input.cpu().numpy()
-#             # Apply DCT using scipy
-#             transformed_np = dct(input_np, type=2, norm="ortho", axis=-1, orthogonalize=True)
-#             # Convert back to PyTorch tensor
-#             output = torch.from_numpy(transformed_np).to(input.device)
-#             return output
+class DCT(Function):
+        @staticmethod
+        def forward(ctx, input):
+            # Convert PyTorch tensor to NumPy array
+            input_np = input.cpu().numpy()
+            # Apply DCT using scipy
+            transformed_np = dct(input_np, type=2, norm="ortho", axis=-1, orthogonalize=True)
+            # Convert back to PyTorch tensor
+            output = torch.from_numpy(transformed_np).to(input.device)
+            return output
 
-#         @staticmethod
-#         def backward(ctx, grad_output):
-#             # Convert gradient to NumPy array
-#             grad_output_np = grad_output.cpu().numpy()
-#             # Apply IDCT using scipy
-#             grad_input_np = idct(grad_output_np, type=2, norm='ortho', axis=-1, orthogonalize=True)
-#             # Convert back to PyTorch tensor
-#             grad_input = torch.from_numpy(grad_input_np).to(grad_output.device)
-#             return grad_input
+        @staticmethod
+        def backward(ctx, grad_output):
+            # Convert gradient to NumPy array
+            grad_output_np = grad_output.cpu().numpy()
+            # Apply IDCT using scipy
+            grad_input_np = idct(grad_output_np, type=2, norm='ortho', axis=-1, orthogonalize=True)
+            # Convert back to PyTorch tensor
+            grad_input = torch.from_numpy(grad_input_np).to(grad_output.device)
+            return grad_input
 
 
-class IDCT(Function):
-    @staticmethod
-    def forward(ctx, input):
-        # Convert PyTorch tensor to NumPy array
-        input_np = input.cpu().numpy()
-        # Apply IDCT using scipy
-        transformed_np = idct(input_np, type=2, axis=-1)
-        # Convert back to PyTorch tensor
-        output = torch.from_numpy(transformed_np).to(input.device)
-        return output
+# class IDCT(Function):
+#     @staticmethod
+#     def forward(ctx, input):
+#         # Convert PyTorch tensor to NumPy array
+#         input_np = input.cpu().numpy()
+#         # Apply IDCT using scipy
+#         transformed_np = idct(input_np, type=2, axis=-1)
+#         # Convert back to PyTorch tensor
+#         output = torch.from_numpy(transformed_np).to(input.device)
+#         return output
 
-    @staticmethod
-    def backward(ctx, grad_output):
-        # Convert gradient to NumPy array
-        grad_output_np = grad_output.cpu().numpy()
-        # Apply DCT using scipy
-        grad_input_np = dct(grad_output_np, type=2, axis=-1)
-        # Convert back to PyTorch tensor
-        grad_input = torch.from_numpy(grad_input_np).to(grad_output.device)
-        return grad_input
+#     @staticmethod
+#     def backward(ctx, grad_output):
+#         # Convert gradient to NumPy array
+#         grad_output_np = grad_output.cpu().numpy()
+#         # Apply DCT using scipy
+#         grad_input_np = dct(grad_output_np, type=2, axis=-1)
+#         # Convert back to PyTorch tensor
+#         grad_input = torch.from_numpy(grad_input_np).to(grad_output.device)
+#         return grad_input
 
 
 class FFN(nn.Module):
@@ -128,13 +128,13 @@ class Model(nn.Module):
         x = F.conv1d(input=x, weight=self.low_pass_filter, stride=2, groups=self.channels)
 
         ## Cosine Transform
-        x = x @ self.dct_matrix
+        x = DCT.apply(x)
 
         ## Prediction
         out = self.layer_lo(x)
 
-        out = self.conv1x1(out)
-        
+        # out = self.conv1x1(out)
+
         out = out.permute(0,2,1) + seq_mean
 
         return out.permute(0,2,1) # [Batch, Output length, Channel]
