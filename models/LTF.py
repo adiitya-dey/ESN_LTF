9c88ff5b5063024e5a9f4d989f342a970c44ffce
aditya.dey <adiitya.dey@gmail.com>
2024-11-16 12:24:52 +0100

    testing

--- models/LTF.py
+++ models/LTF.py
@@ -116,10 +116,12 @@ class Model(nn.Module):
         # x = F.pad(x, (0,1))
         x = F.conv1d(input=x, weight=self.low_pass_filter, stride=2, groups=self.channels)
 
+        batch, channel, length = x.shape
+
         ##Cosine Transform
-        x = x.view(batch, self.channels, int(512//16), 16)
+        x = x.view(batch, channel, int(length//16), 16)
         x = DCT.apply(x) / x.shape[-1]
-        x = x.view(batch, self.channels, -1)
+        x = x.view(batch, channel, -1)
 
         
 
