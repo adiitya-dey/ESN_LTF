ad5e19ca3132e0c544039961eca0d5b245f6e1ea
aditya dey <adiitya.dey@gmail.com>
2024-12-12 11:50:06 +0100

    All bash scripts ready.

--- run_all.sh
+++ run_all.sh
@@ -1,12 +1,57 @@
 #!/bin/bash
 
-sh scripts/SparseTSF/etth1.sh;
-sh scripts/SparseTSF/etth2.sh;
-sh scripts/SparseTSF/electricity.sh;
-sh scripts/SparseTSF/traffic.sh;
-sh scripts/SparseTSF/ettm1.sh;
-sh scripts/SparseTSF/ettm2.sh;
-sh scripts/SparseTSF/weather.sh;
+#HaarDCT
+sh scripts/multivariate/HaarDCT/etth1.sh
+sh scripts/multivariate/HaarDCT/etth2.sh
+sh scripts/multivariate/HaarDCT/ettm1.sh
+sh scripts/multivariate/HaarDCT/ettm2.sh
+sh scripts/multivariate/HaarDCT/electricity.sh
+sh scripts/multivariate/HaarDCT/weather.sh
+sh scripts/multivariate/HaarDCT/traffic.sh
 
+#SparseTSF
+sh scripts/multivariate/SparseTSF/etth1.sh
+sh scripts/multivariate/SparseTSF/etth2.sh
+sh scripts/multivariate/SparseTSF/ettm1.sh
+sh scripts/multivariate/SparseTSF/ettm2.sh
+sh scripts/multivariate/SparseTSF/electricity.sh
+sh scripts/multivariate/SparseTSF/weather.sh
+sh scripts/multivariate/SparseTSF/traffic.sh
+
+#PatchTST
+sh scripts/multivariate/PatchTST/etth1.sh
+sh scripts/multivariate/PatchTST/etth2.sh
+sh scripts/multivariate/PatchTST/ettm1.sh
+sh scripts/multivariate/PatchTST/ettm2.sh
+sh scripts/multivariate/PatchTST/electricity.sh
+sh scripts/multivariate/PatchTST/weather.sh
+sh scripts/multivariate/PatchTST/traffic.sh
+
+#FreTS
+sh scripts/multivariate/FreTS/etth1.sh
+sh scripts/multivariate/FreTS/etth2.sh
+sh scripts/multivariate/FreTS/ettm1.sh
+sh scripts/multivariate/FreTS/ettm2.sh
+sh scripts/multivariate/FreTS/electricity.sh
+sh scripts/multivariate/FreTS/weather.sh
+sh scripts/multivariate/FreTS/traffic.sh
+
+#NLinear
+sh scripts/multivariate/NLinear/etth1.sh
+sh scripts/multivariate/NLinear/etth2.sh
+sh scripts/multivariate/NLinear/ettm1.sh
+sh scripts/multivariate/NLinear/ettm2.sh
+sh scripts/multivariate/NLinear/electricity.sh
+sh scripts/multivariate/NLinear/weather.sh
+sh scripts/multivariate/NLinear/traffic.sh
+
+#DLinear
+sh scripts/multivariate/DLinear/etth1.sh
+sh scripts/multivariate/DLinear/etth2.sh
+sh scripts/multivariate/DLinear/ettm1.sh
+sh scripts/multivariate/DLinear/ettm2.sh
+sh scripts/multivariate/DLinear/electricity.sh
+sh scripts/multivariate/DLinear/weather.sh
+sh scripts/multivariate/DLinear/traffic.sh
 
 
